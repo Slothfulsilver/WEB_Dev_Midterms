0c1d0625d23d40a53b1e98c352f45d1d071dcf6d
RLG-UP <0262146@up.edu.mx>
2024-10-17 12:02:56 -0600

    Uploading ready api call to repo

--- Midterm 2/server.js
+++ Midterm 2/server.js
@@ -1,4 +1,6 @@
+//Setup for the Node+Express project
 const express = require('express');
+const https = require('https');
 const app = express();
 
 app.use(express.json());
@@ -7,12 +9,46 @@ app.use(express.static("public"));
 app.engine("ejs", require("ejs").renderFile);
 app.set("view engine", "ejs");
 
+
+//GET and POST functions for the root
+
+//Including the api call on the GET method for the root
 app.route('/')
-  .get((req, res) => {
-    res.render("home");
+  .get(async (req, res) => {
+    //Var with URL for API call
+    var swCall = `https://rawcdn.githack.com/akabab/starwars-api/0.2.1/api/all.json`;
+
+    await https.get(swCall, (response=>{
+      console.log("Got a response from swCall");
+      console.log(response.statusCode);
+      
+      var responseContent="";
+      response.on("data", (data)=>{
+        console.log(responseContent);
+        responseContent += data;
+      });
+      
+      response.on("end", ()=>{
+          var jsonResp = JSON.parse
+        (responseContent);
+
+        var params = {
+          jsonResp
+        };
+        
+        console.log("SANDMAN");
+        res.render("home", params);
+
+      }).on("error", (e)=>{
+        res.send("Error: $(e.message");
+      });
+    }));
   })
+
   .post((req, res) => {
     res.send();
   });
 
+
+//Listening port 3000
 app.listen(3000);
